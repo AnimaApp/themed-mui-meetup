4b687ef7c980f39039fd302697f69d4970456fb7
orarbel <orarbel@gmail.com>
2023-01-01 14:55:43 +0200

    .

--- src/stories/MUIButton.stories.jsx
+++ src/stories/MUIButton.stories.jsx
@@ -1,38 +1,109 @@
 import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Icon from "@mui/material/Icon";
 
 export default {
   title: "Material UI/Button",
   component: Button,
   argTypes: {
-    variant: {
-      control: { type: "select", options: ["text", "outlined", "contained"] },
+    state: {
+      control: { type: "select", options: ["default", "hover", "active"] },
     },
     color: {
       control: {
         type: "select",
-        options: [
-          "primary",
-          "secondary",
-          "success",
-          "error",
-          "info",
-          "warning",
-        ],
+        options: ["primary", "success"],
       },
     },
     size: {
-      control: { type: "select", options: ["small", "medium", "large"] },
+      control: { type: "select", options: ["small", "medium"] },
     },
+    leftIcon: { control: "text" },
+    rightIcon: { control: "text" },
+    showLeftIcon: { control: "boolean" },
+    showRightIcon: { control: "boolean" },
     disabled: { control: "boolean" },
   },
 };
 
-const Template = (args) => <Button {...args}>{args.label}</Button>;
+// const Template = (args) => {
+//   if (args.leftIcon && args.label) return <Button className={args.state} {...args} variant="contained" startIcon={<Icon>{args.icon}</Icon>}>{args.label}</Button>;
+//   else if (args.leftIcon) {
+//     return <IconButton {...args}><Icon>{args.icon}</Icon></IconButton>;
+//   } return <Button className={args.state} variant="contained" {...args}>{args.label}</Button>
+
+// };
+
+// export const MuiButton = Template.bind({});
+// MuiButton.args = {
+//   label: "Text",
+//   state: "default",
+//   size: "medium",
+//   color: "primary",
+//   disabled: false,
+//   leftIcon: false,
+//   icon: "star",
+// };
+
+const Template = (args) => {
+  if (args.showLeftIcon && args.label && args.showRightIcon)
+    return (
+      <Button
+        className={args.state}
+        {...args}
+        variant="contained"
+        startIcon={<Icon>{args.leftIcon}</Icon>}
+        endIcon={<Icon>{args.rightIcon}</Icon>}
+      >
+        {args.label}
+      </Button>
+    );
+  if (args.showLeftIcon && args.label)
+    return (
+      <Button
+        className={args.state}
+        {...args}
+        variant="contained"
+        startIcon={<Icon>{args.leftIcon}</Icon>}
+      >
+        {args.label}
+      </Button>
+    );
+    if (args.showRightIcon && args.label)
+    return (
+      <Button
+        className={args.state}
+        {...args}
+        variant="contained"
+        endIcon={<Icon>{args.rightIcon}</Icon>}
+      >
+        {args.label}
+      </Button>
+    );
+  else if (args.showLeftIcon) {
+    return (
+      <IconButton {...args}>
+        <Icon>{args.icon}</Icon>
+      </IconButton>
+    );
+  } else {
+    return (
+      <Button className={args.state} variant="contained" {...args}>
+        {args.label}
+      </Button>
+    );
+  }
+};
 
 export const MuiButton = Template.bind({});
 MuiButton.args = {
-  label: "Button",
-  variant: "contained",
+  label: "Text",
+  state: "default",
+  size: "medium",
   color: "primary",
   disabled: false,
+  showLeftIcon: false,
+  showRightIcon: false,
+  leftIcon: "star",
+  rightIcon: "star",
 };
--- src/theme.js
+++ src/theme.js
@@ -1,25 +1,84 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
+import tokens from "../design-tokens.json";
+
+const daisy = tokens["Daisycols"];
 
 export const theme = createTheme({
   palette: {
     primary: {
-      main: "#ea1b3d"
-    }
+      main: daisy["primary_default"].value,
+    },
+    success: {
+      main: daisy["success_default"].value,
+    },
   },
   typography: {
-    fontFamily: 'Nunito Sans, sans-serif',
+    fontFamily: "Poppins",
     button: {
-      textTransform: 'none'
-    }
+      textTransform: "none",
+    },
   },
   shape: {
-    borderRadius: 18
+    borderRadius: 5,
   },
   components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: Poppins;
+          font-size: 14px;
+          font-weight: 600;
+          line-height: 20px;
+          letter-spacing: 0px;
+          text-align: center;
+        }
+      `,
+    },
+    MuiIcon: {
+      defaultProps: {
+        baseClassName: "material-icons",
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: ({ ownerState }) => {
+          const overrides = {};
+          if (ownerState.size === "medium") {
+            overrides["padding"] = "10px 20px";
+          }
+          if (ownerState.size === "small") {
+            overrides["padding"] = "5px 10px";
+          }
+          overrides["color"] = "white";
+          console.log(ownerState);
+          if (ownerState.disabled) {
+            overrides["color"] = 'rgba(28, 47, 73, 0.2)';
+            overrides["backgroundColor"] = 'rgba(76, 132, 255, 0.05)';
+          } else {
+            overrides["backgroundColor"] =
+              daisy[`${ownerState.color}_${ownerState.state}`].value;
+            overrides["boxShadow"] = "0px 4px 10px 0px rgba(76, 132, 255, 0.4)";
+          }
+          overrides["&:hover"] = {
+            backgroundColor: daisy[`${ownerState.color}_hover`].value,
+            boxShadow: "0px 4px 10px 0px rgba(76, 132, 255, 0.4)"
+          }
+          overrides["&:active"] = {
+            backgroundColor: daisy[`${ownerState.color}_active`].value,
+            boxShadow: "0px 4px 10px 0px rgba(76, 132, 255, 0.4)"
+          }
+          overrides["&:disabled"] = {
+            color: 'rgba(28, 47, 73, 0.2)',
+            backgroundColor: 'rgba(76, 132, 255, 0.05)'
+          }
+          return overrides;
+        },
+      },
+    },
     MuiButtonBase: {
       defaultProps: {
-        disableRipple: true, 
+        disableRipple: true,
       },
     },
   },
-})
+});
